5223364ad52524eb56964e0eaf7ab940a9f95c0c
Emilio Arcioni <emilio@arcioni.com.ar>
2022-01-15 01:23:05 -0300

    Telegram bot integration

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 This is a personal mini project to calculate your monthly working rate based on yours worked tracked by Harvest.
 
+It has an integration with Telegram in order that you can check your worked hours on a bot's chat.
+If you send the command `/estimate` it will return your billing hours.
+
 ## Getting Started
 
 - Clone this repo and run `yarn install`
@@ -12,3 +15,5 @@ In order to run properly this script you will need:
 - Nodejs
 - Yarn
 - Nodemon (not mandatory)
+
+This [article](https://www.siteguarding.com/en/how-to-get-telegram-bot-api-token) could be useful to setup an initialize a Bot in Telegram.
--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 const axios = require("axios");
+const TelegramBot = require("node-telegram-bot-api");
 
-const { HARVEST_ACCOUNT_ID, HARVEST_ACCESS_TOKEN, HOUR_RATE, WORKING_HOURS } = process.env;
+const {
+  HARVEST_ACCOUNT_ID,
+  HARVEST_ACCESS_TOKEN,
+  HOUR_RATE,
+  WORKING_HOURS,
+  TELEGRAM_CHAT_ID,
+  TELEGRAM_BOT_TOKEN,
+} = process.env;
+
+const formatCurrency = (amount) => {
+  const numberFormat = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  return numberFormat.format(amount);
+};
 
 const formatDate = (date) => {
   const year = date.getFullYear();
-  const month = `${date.getMonth() < 10 ? '0' : ''}${date.getMonth() + 1}`;
-  const day = `${date.getDate() < 10 ? '0' : ''}${date.getDate()}`;
+  const month = `${date.getMonth() < 10 ? "0" : ""}${date.getMonth() + 1}`;
+  const day = `${date.getDate() < 10 ? "0" : ""}${date.getDate()}`;
 
   return year + month + day;
 };
@@ -24,31 +40,37 @@ const workingDaysLeft = () => {
   const { lastDay } = getDates();
 
   let count = 0;
-  for(let i = date.getDate() + 1; i <= lastDay.getDate(); i ++) {
+  for (let i = date.getDate() + 1; i <= lastDay.getDate(); i++) {
     const day = new Date(date.getFullYear(), date.getMonth(), i).getDay();
-    count += (day !== 0 && day !== 6 ? 1 : 0);
+    count += day !== 0 && day !== 6 ? 1 : 0;
   }
 
   return count;
 };
 
-const run = async () => {
+const calculate = async () => {
   const { firstDay, lastDay } = getDates();
-  
+
   try {
-    const response = await axios.get(`https://api.harvestapp.com/v2/reports/time/tasks?from=${formatDate(firstDay)}&to=${formatDate(lastDay)}`, {
-      headers: {
-        "Authorization": `Bearer ${HARVEST_ACCESS_TOKEN}`,
-        "Harvest-Account-ID": HARVEST_ACCOUNT_ID
+    const response = await axios.get(
+      `https://api.harvestapp.com/v2/reports/time/tasks?from=${formatDate(
+        firstDay
+      )}&to=${formatDate(lastDay)}`,
+      {
+        headers: {
+          Authorization: `Bearer ${HARVEST_ACCESS_TOKEN}`,
+          "Harvest-Account-ID": HARVEST_ACCOUNT_ID,
+        },
       }
-    });
+    );
     let workedHours = 0;
-    response.data.results.map(result => {
+    response.data.results.map((result) => {
       workedHours += result.billable_hours;
     });
 
     const workedIncome = workedHours * parseInt(HOUR_RATE);
-    const estimatedHours = (workedHours + (workingDaysLeft() * parseInt(WORKING_HOURS)));
+    const estimatedHours =
+      workedHours + workingDaysLeft() * parseInt(WORKING_HOURS);
     const estimatedIncome = estimatedHours * parseInt(HOUR_RATE);
 
     return { workedHours, workedIncome, estimatedHours, estimatedIncome };
@@ -57,4 +79,34 @@ const run = async () => {
   }
 };
 
-run().then(data => console.log(data));
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, {
+  polling: true,
+});
+
+bot.on("message", (msg) => {
+  if (TELEGRAM_CHAT_ID != msg.chat.id) return;
+
+  if (msg.entities) {
+    msg.entities.map((entity) => {
+      if (
+        entity.type === "bot_command" &&
+        msg.text.slice(entity.offset, entity.offset + entity.length) ===
+          "/estimate"
+      ) {
+        calculate().then((data) => {
+          bot.sendMessage(
+            TELEGRAM_CHAT_ID,
+            `*Worked current month*\n⏰${
+              data.workedHours
+            } hs 💵${formatCurrency(
+              data.workedIncome
+            )}\n*Monthly estimate*\n⏰${
+              data.estimatedHours
+            } hs 💵${formatCurrency(data.estimatedIncome)}`,
+            { parse_mode: "markdown" }
+          );
+        });
+      }
+    });
+  }
+});
